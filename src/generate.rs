3b25a38f6a6762462f1e7e8359e082d5f07bac84
codermarcos <coder.marcos@gmail.com>
2020-07-12 00:46:16 -0300

    :new: get timestamp to get time to order

--- src/generate.rs
+++ src/generate.rs
@@ -1,4 +1,13 @@
+use std::time::SystemTime;
+use std::time::UNIX_EPOCH;
+
 use rand::prelude::random;
+
+pub fn time() -> std::time::Duration {
+	SystemTime::now()
+		.duration_since(UNIX_EPOCH)
+		.expect("Tempo esta fora do range")
+}
 
 pub fn vec(size: usize) -> Vec<u8> {
 	let mut array: Vec<u8> = Vec::with_capacity(size);
