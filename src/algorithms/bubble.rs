ca0f3d30b93a54af0abf2e36e42b47052b0f7fee
codermarcos <coder.marcos@gmail.com>
2020-07-12 00:53:30 -0300

    :new: create bubble algorithm

--- src/algorithms/bubble.rs
+++ src/algorithms/bubble.rs
@@ -1,3 +1,26 @@
+
+fn algorithm(vector: Vec<u8>) -> Vec<u8> {
+	let mut ordened: bool = false;
+	let mut result: Vec<u8> = vector;
+
+	while !ordened {
+		ordened = true;
+
+		for n in 0..result.len() {
+			if n + 1 < result.len() && result[n] > result[n + 1] {
+				let n1: u8 = result[n];
+				let n2: u8 = result[n + 1];
+
+				result[n] = n2;
+				result[n + 1] = n1;
+				ordened = false;
+			}
+		}
+	}
+
+	result
+}
+
 #[cfg(test)]
 mod tests {
 
