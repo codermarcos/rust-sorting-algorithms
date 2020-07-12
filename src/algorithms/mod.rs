ae396450dd2048d2010a852a1a4da12a71447079
codermarcos <coder.marcos@gmail.com>
2020-07-12 01:02:51 -0300

    :new: create handler to choose algorithm

--- src/algorithms/mod.rs
+++ src/algorithms/mod.rs
@@ -1,5 +1,8 @@
 pub mod bubble;
 pub mod insertion;
+pub mod quick;
+pub mod selection;
+
 pub enum Algorithm {
 	Quick,
 	Bubble,
@@ -22,4 +25,19 @@ pub struct SortResult {
 	pub vector: std::vec::Vec<u8>,
 	pub algorithm: Algorithm,
 	pub duration: u128,
+}
+
+pub fn handle_algorithm(input_algorithm: &str, array: Vec<u8>) -> Vec<SortResult> {
+	match input_algorithm {
+		"1" => vec![quick::sort(array)],
+		"2" => vec![bubble::sort(array)],
+		"3" => vec![insertion::sort(array)],
+		"4" => vec![selection::sort(array)],
+		_ => vec![
+			quick::sort(array.clone()),
+			bubble::sort(array.clone()),
+			insertion::sort(array.clone()),
+			selection::sort(array.clone()),
+		],
+	}
 }
